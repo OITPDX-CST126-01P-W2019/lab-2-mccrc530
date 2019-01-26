b90f7bb11e509b7c3004cad057806b2611f39a65
Connor <mccrite.connor@gmail.com>
2019-01-26 09:56:17 -0800

    Added Bottle class

---
+++ W19_CST126_2/Bottle.cpp
@@ -0,0 +1,30 @@
+#include "pch.h"
+#include "Bottle.h"
+
+
+Bottle::Bottle()
+{
+	color = "";
+	type = "";
+	age = 0;
+	value = 0.0;
+}
+
+Bottle::Bottle(std::string const & inputColor, std::string const & inputType, int const inputAge, double const inputValue)
+{
+	initializeBottle(inputColor, inputType, inputAge, inputValue);
+}
+
+void Bottle::initializeBottle(std::string const & inputColor, std::string const & inputType, int const inputAge, double const inputValue)
+{
+	color = inputColor;
+	type = inputType;
+	age = inputAge;
+	value = inputValue;
+}
+
+void Bottle::printBottle() const
+{
+	std::cout << std::setw(10);
+	std::cout << color << type << age << value << std::endl;
+}
---
+++ W19_CST126_2/Bottle.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include <iomanip>
+class Bottle
+{
+public:
+	Bottle();
+	Bottle(std::string const & inputColor, std::string const & inputType, int const inputAge, double const inputValue);
+	void printBottle() const;
+	
+private:
+	std::string color, type;
+	int age;
+	double value;
+	void initializeBottle(std::string const & inputColor, std::string const & inputType, int const inputAge, double const inputValue);
+};
+
